da921cd6b8b86d3fa7b6ba864a368c112dc986bf
Greg Cosgrove <gregcosgrove@gmail.com>
2014-05-19 11:15:14 -0500

    First Commit

---
+++ lib/rps.rb
@@ -0,0 +1,8 @@
+module RPS
+end
+
+require_relative './rps/db.rb'
+require_relative './rps/invites.rb'
+require_relative './rps/match.rb'
+require_relative './rps/round.rb'
+require_relative './rps/user.rb'
---
+++ lib/rps/invites.rb
@@ -0,0 +1,2 @@
+class RPS::Invites
+end
